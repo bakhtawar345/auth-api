7978347aad0c2af9808b8fb2e2ff4a5ea1aa37d9
bakhtawar345 <bfaisal@oberlin.edu>
2026-06-23 11:08:03 +0500

    initial comit - auth api set up

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const mongoose = require('mongoose');
+const dotenv= require('dotenv');
+
+dotenv.config(); //load the .env file
+
+const app = express(); //create the express application
+
+app.use(express.json());
+
+const authRoutes = require('./routes/auth'); //importing route files
+app.use('/auth', authRoutes); // any request that starts with /auth is handed over to authRoutes
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err)=> console.log('Connection failed: ', err));
+
+const PORT= process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
